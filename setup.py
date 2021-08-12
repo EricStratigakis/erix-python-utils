f1c462546105d87d45d345608edc1825bad7015f
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-08-12 13:21:16 -0400

    minimum

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="erix-python-utils",
+    version='0.0.1',
+    author="Eric Stratigakis",
+    author_email="enstrati@uwaterloo.ca",
+    description='All of my utilities and tools in one place',
+    py_modules=["helloworld"],
+    package_dir={'':'src'},
+)
