1dff53d79b9b0fae1ca95140952208effc0a0fc1
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-08-12 13:29:29 -0400

    added classifiers

--- setup.py
+++ setup.py
@@ -8,4 +8,10 @@ setup(
     description='All of my utilities and tools in one place',
     py_modules=["helloworld"],
     package_dir={'':'src'},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
