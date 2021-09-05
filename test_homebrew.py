67aa07657b585815087f02c220fdd0e668caa733
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-09-05 01:50:37 -0400

    added config

---
+++ test_homebrew.py
@@ -0,0 +1,15 @@
+from unittest import main, TestCase
+
+class HomeBrewTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_unittest_runs(slef):
+        pass
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+if __name__ == "__main__":
+    main()
