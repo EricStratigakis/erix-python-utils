de360d51098434059a61b6e686bf01e6921799fe
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-09-06 16:11:37 -0400

    added pypi creds

--- setup.py
+++ setup.py
@@ -1,30 +1,15 @@
-from os import getenv
-from sys import exit
-
 from setuptools import setup
-from setuptools.command.install import install
 
-# circleci.py version
-VERSION = "1.1.1"
+VERSION = "0.0.10"
 
 def readme():
     """print long description"""
     with open("README.md", "r") as fh:
         return fh.read()
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
-
-    def run(self):
-        tag = getenv('CIRCLE_TAG')
-
-        if tag != VERSION:
-            info = f"Git tag: {tag} does not match the version of this app: {VERSION}"
-            exit(info)
 
 setup(
     name="erix-python-utils",
-    version="0.0.10",
+    version=VERSION,
     author="Eric Stratigakis",
     author_email="enstrati@uwaterloo.ca",
     description="All of my utilities and tools in one place",
@@ -40,7 +25,4 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=["requests"],
     python_requires='>=3',
-    cmdclass={
-        'verify': VerifyVersionCommand,
-    }
 )
